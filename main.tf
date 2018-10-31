636be71bc4fbf47e377e29e8838531b4edc615c4
mjcmtb <mikejnc@gmail.com>
2018-10-31 10:08:04 -0700

    Initial Commit

---
+++ main.tf
@@ -0,0 +1,149 @@
+locals {
+  tags = {
+    ServiceProvider = "Rackspace"
+    Environment     = "${var.environment}"
+  }
+
+  alarm_emergency = "${var.enable_rackspace_ticket ? "arn:aws:sns:${data.aws_region.current_region.name}:${data.aws_caller_identity.current_account.account_id}:rackspace-support-emergency" : ""}"
+  alarm_urgent    = "${var.enable_rackspace_ticket ? "arn:aws:sns:${data.aws_region.current_region.name}:${data.aws_caller_identity.current_account.account_id}:rackspace-support-urgent" : ""}"
+}
+
+data "aws_region" "current_region" {}
+data "aws_caller_identity" "current_account" {}
+
+resource "aws_redshift_subnet_group" "redshift_subnet_group" {
+  name       = "${lower("${var.resource_name}-subnetgroup")}"
+  subnet_ids = ["${var.subnets}"]
+
+  tags = "${merge(
+    local.tags,
+    var.additional_tags
+)}"
+}
+
+data "aws_iam_policy_document" "redshift_assume_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+
+    principals {
+      identifiers = ["redshift.amazonaws.com"]
+      type        = "Service"
+    }
+  }
+}
+
+resource "aws_iam_role" "redshift_role" {
+  name               = "${var.resource_name}-Role"
+  assume_role_policy = "${data.aws_iam_policy_document.redshift_assume_policy.json}"
+}
+
+resource "aws_iam_role_policy_attachment" "redshift_policy_attach" {
+  count      = "${var.count_cluster_role_managed_policy_arns}"
+  policy_arn = "${element(var.cluster_role_managed_policy_arns, count.index)}"
+}
+
+resource "aws_redshift_parameter_group" "redshift_parameter_group" {
+  name        = "${lower("${var.resource_name}-parametergroup")}"
+  description = "${join("-", list(var.environment, "parametergroup"))}"
+  family      = "redshift-${var.cluster_version}"
+
+  parameter {
+    name  = "enable_user_activity_logging"
+    value = "true"
+  }
+}
+
+resource "aws_redshift_cluster" "redshift_cluster" {
+  cluster_identifier                  = "${lower("${var.resource_name}-cluster")}"
+  cluster_type                        = "${var.cluster_type}"
+  cluster_version                     = "${var.cluster_version}"
+  availability_zone                   = "${var.availability_zone}"
+  publicly_accessible                 = "${var.publicly_accessible}"
+  elastic_ip                          = "${var.use_elastic_ip && var.publicly_accessible ? var.elastic_ip : ""}"
+  master_username                     = "${var.master_username}"
+  master_password                     = "${var.master_password}"
+  encrypted                           = "${var.storage_encrypted}"
+  snapshot_identifier                 = "${var.redshift_snapshot_identifier}"
+  vpc_security_group_ids              = ["${var.security_group_list}"]
+  iam_roles                           = ["${aws_iam_role.redshift_role.arn}"]
+  allow_version_upgrade               = "${var.allow_version_upgrade}"
+  kms_key_id                          = "${var.key_id}"
+  automated_snapshot_retention_period = "${var.backup_retention_period}"
+  preferred_maintenance_window        = "${var.preferred_maintenance_window}"
+  node_type                           = "${var.redshift_instance_class}"
+  cluster_parameter_group_name        = "${aws_redshift_parameter_group.redshift_parameter_group.name}"
+  port                                = "${var.port}"
+  database_name                       = "${var.db_name}"
+  number_of_nodes                     = "${var.number_of_nodes}"
+  cluster_subnet_group_name           = "${aws_redshift_subnet_group.redshift_subnet_group.name}"
+  final_snapshot_identifier           = "${var.final_snapshot_identifier}"
+  skip_final_snapshot                 = "${var.skip_final_snapshot}"
+
+  tags = "${merge(
+    local.tags,
+    var.additional_tags
+)}"
+}
+
+resource "aws_route53_record" "redshift_internal_record_set" {
+  count   = "${var.create_route53_record ? 1 : 0}"
+  name    = "${var.internal_record_name}.${var.internal_zone_name}"
+  type    = "CNAME"
+  zone_id = "${var.internal_zone_id}"
+  records = ["${aws_redshift_cluster.redshift_cluster.endpoint}"]
+  ttl     = 300
+}
+
+resource "aws_cloudwatch_metric_alarm" "redshift_cpu_alarm_high" {
+  alarm_name          = "${var.resource_name}-CPUAlarmHigh"
+  alarm_description   = "Alarm if ${aws_redshift_cluster.redshift_cluster.id} CPU > ${var.cw_cpu_threshold}% for 5 minutes"
+  period              = 60
+  comparison_operator = "GreaterThanThreshold"
+  statistic           = "Average"
+  threshold           = "${var.cw_cpu_threshold}"
+  evaluation_periods  = 5
+  namespace           = "AWS/Redshift"
+  metric_name         = "CPUUtilization"
+
+  dimensions {
+    ClusterIdentifier = "${aws_redshift_cluster.redshift_cluster.id}"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "redshift_cluster_health_Ticket" {
+  alarm_name          = "${var.resource_name}-CluterHealthTicket"
+  alarm_description   = "Cluster has entered unhealthy state, creating ticket"
+  period              = 60
+  comparison_operator = "LessThanThreshold"
+  statistic           = "Average"
+  threshold           = 1
+  evaluation_periods  = 5
+  namespace           = "AWS/Redshift"
+  metric_name         = "HealthStatus"
+  alarm_actions       = ["${compact(list(local.alarm_emergency))}"]
+  ok_actions          = ["${compact(list(local.alarm_emergency))}"]
+
+  dimensions {
+    ClusterIdentifier = "${aws_redshift_cluster.redshift_cluster.id}"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "redshift_free_storage_space_ticket" {
+  alarm_name          = "${var.resource_name}-FreeStorageSpaceTicket"
+  alarm_description   = "Consumed storage space has risen above threshold, sending email notification"
+  period              = 60
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  statistic           = "Average"
+  threshold           = "${var.cw_percentage_disk_used}"
+  evaluation_periods  = 30
+  namespace           = "AWS/Redshift"
+  metric_name         = "PercentageDiskSpaceUsed"
+  unit                = "Percent"
+  alarm_actions       = ["${compact(list(local.alarm_urgent))}"]
+  ok_actions          = ["${compact(list(local.alarm_urgent))}"]
+
+  dimensions {
+    ClusterIdentifier = "${aws_redshift_cluster.redshift_cluster.id}"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,216 @@
+#<General vars>
+variable "additional_tags" {
+  description = "Additional tags to be added to the RedShift module resources"
+  type        = "map"
+  default     = {}
+}
+
+variable "resource_name" {
+  description = "The name to be used for resources provisioned by this module"
+  type        = "string"
+}
+
+#<\General vars>
+
+#<Cluster vars>
+variable "availability_zone" {
+  description = "Availability zone in which to initially provision Redshift."
+  default     = ""
+  type        = "string"
+}
+
+variable "allow_version_upgrade" {
+  description = "Indicates that engine upgrades will be applied automatically to the Redshift cluster during the maintenance window"
+  default     = true
+  type        = "string"
+}
+
+variable "backup_retention_period" {
+  description = "The number of days for which automated backups are retained. Setting this parameter to a positive number enables backups. Setting this parameter to 0 disables automated backups"
+  default     = 1
+  type        = "string"
+}
+
+variable "cluster_type" {
+  description = "Create a single-node or multi-node Redshift cluster"
+  default     = "single-node"
+  type        = "string"
+}
+
+variable "cluster_version" {
+  description = "Redshift Engine Version"
+  default     = "1.0"
+  type        = "string"
+}
+
+variable "db_name" {
+  description = "Name of initial Redshift database"
+  default     = "myredshift"
+  type        = "string"
+}
+
+variable "elastic_ip" {
+  description = "The Elastic IP (EIP) address for the cluster (must have publicly accessible enabled)"
+  default     = ""
+  type        = "string"
+}
+
+variable "final_snapshot_identifier" {
+  description = "If provided, a final snapshot will be created immediately before deleting the cluster."
+  default     = "myfinalredshiftsnapshot"
+  type        = "string"
+}
+
+variable "key_id" {
+  description = "The ID of the AWS Key Management Service (AWS KMS) key that you want to use to encrypt data in the cluster"
+  default     = ""
+  type        = "string"
+}
+
+variable "master_username" {
+  description = "The name of master user for the Redshift instance"
+  type        = "string"
+}
+
+variable "master_password" {
+  description = "The master password for the Redshift Instance"
+  type        = "string"
+}
+
+variable "number_of_nodes" {
+  description = "If ClusterType is single-node, this parameter is ignored. If ClusterType is multi-node, NumberOfNodes must be >= 2."
+  default     = 1
+  type        = "string"
+}
+
+variable "port" {
+  description = "The port number on which the database accepts connections"
+  default     = 5439
+  type        = "string"
+}
+
+variable "preferred_maintenance_window" {
+  description = "The daily time range during which automated backups are created if automated backups are enabled"
+  default     = "Sun:05:00-Sun:07:00"
+  type        = "string"
+}
+
+variable "publicly_accessible" {
+  description = "Indicates whether the Redshift cluster is an Internet-facing cluster"
+  default     = false
+  type        = "string"
+}
+
+variable "redshift_instance_class" {
+  description = "The compute and memory capacity of the nodes within the Redshift cluster"
+  default     = "dc1.large"
+  type        = "string"
+}
+
+variable "redshift_snapshot_identifier" {
+  description = "The name of the snapshot from which to create a new cluster"
+  default     = ""
+  type        = "string"
+}
+
+variable "security_group_list" {
+  description = "A list of EC2 security groups to assign to this resource."
+  default     = []
+  type        = "list"
+}
+
+variable "skip_final_snapshot" {
+  description = "Skip final snapshot before deleting the cluster. true or false."
+  default     = false
+  type        = "string"
+}
+
+variable "storage_encrypted" {
+  description = "Specifies whether the Redshift cluster is encrypted"
+  default     = false
+  type        = "string"
+}
+
+variable "subnets" {
+  description = "Subnets for use with this Redshift cluster"
+  default     = []
+  type        = "list"
+}
+
+variable "use_elastic_ip" {
+  description = "Instruct module to use provided Elastic IP Address"
+  default     = false
+  type        = "string"
+}
+
+#<\Cluster vars>
+
+#<Route53 vars>
+variable "create_route53_record" {
+  description = "Specifies whether or not to create a route53 CNAME record for the redshift endpoint. internal_zone_id, internal_zone_name, and internal_record_name must be provided if set to true. true or false."
+  default     = false
+  type        = "string"
+}
+
+variable "internal_record_name" {
+  description = "Record Name for the new Resource Record in the Internal Hosted Zone"
+  default     = ""
+  type        = "string"
+}
+
+variable "internal_zone_name" {
+  description = "TLD for Internal Hosted Zone"
+  default     = ""
+  type        = "string"
+}
+
+variable "internal_zone_id" {
+  description = "The Route53 Internal Hosted Zone ID"
+  default     = ""
+  type        = "string"
+}
+
+#<\Route53 vars>
+
+#<Cloudwatch vars>
+variable "cw_cpu_threshold" {
+  description = "CloudWatch CPUUtilization Threshold"
+  default     = 90
+  type        = "string"
+}
+
+variable "cw_percentage_disk_used" {
+  description = "CloudWatch Percentage of storage consumed threshold"
+  default     = 90
+  type        = "string"
+}
+
+variable "enable_rackspace_ticket" {
+  description = "Specifies whether alarms will generate Rackspace tickets"
+  default     = false
+  type        = "string"
+}
+
+#<\Cloudwatch vars>
+
+variable "environment" {
+  description = "Application environment for which this network is being created. e.g. Development/Production."
+  default     = "Development"
+  type        = "string"
+}
+
+#<IAM vars>
+variable "cluster_role_managed_policy_arns" {
+  description = "A comma delimited list of IAM policy ARNs for the ClusterRole IAM role.  IAM ARNs can be found within the Policies section of the AWS IAM console."
+  default     = []
+  type        = "list"
+}
+
+variable "count_cluster_role_managed_policy_arns" {
+  description = "Count of provided policy ARNs provided as a list into variable cluster_role_managed_policy_arns. Must be provided if policies are being given in variable cluster_role_managed_policy_arns."
+  default     = 0
+  type        = "string"
+}
+
+#<\IAM vars>
+
