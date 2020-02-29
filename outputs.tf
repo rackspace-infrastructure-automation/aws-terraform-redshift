e90b6cdda02954bb821c3bdb37233def8f09e526
Giovanni Del Valle <giovanni.delvalle@rackspace.com>
2020-02-28 23:27:23 -0600

    sort outputs

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,18 @@
+output "db_port" {
+  description = "Cluster endpoint port number"
+  value       = "${aws_redshift_cluster.redshift_cluster.port}"
+}
 output "jdbc_connection_string" {
   description = "JDBC connection string for cluster"
   value       = "jdbc:redshift://${aws_redshift_cluster.redshift_cluster.endpoint}:${aws_redshift_cluster.redshift_cluster.port}/${aws_redshift_cluster.redshift_cluster.database_name}"
 }
 
-output "db_port" {
-  description = "Cluster endpoint port number"
-  value       = "${aws_redshift_cluster.redshift_cluster.port}"
+output "redshift_address" {
+  description = "Address of database endpoint"
+  value       = "${aws_redshift_cluster.redshift_cluster.endpoint}"
 }
 
 output "redshift_cluster_identifier" {
   description = "Redshift cluster identifier"
   value       = "${aws_redshift_cluster.redshift_cluster.id}"
-}
-
-output "redshift_address" {
-  description = "Address of database endpoint"
-  value       = "${aws_redshift_cluster.redshift_cluster.endpoint}"
 }
